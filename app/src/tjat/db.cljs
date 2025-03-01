cb617635fe02e862def5b42eae45b8bdc03d1402
Mikkel Gravgaard <mikkel@klokke.dk>
2025-03-01 20:49:44 +0100

    WIP

--- app/src/tjat/db.cljs
+++ app/src/tjat/db.cljs
@@ -9,10 +9,10 @@
 ;; we don't need no hooks!
 
 (defn init-instant-db [{:keys [app-id on-error on-success]
-                        :or {on-error #(js/console.warn (.-error %))
-                             on-success #(js/console.log (.-data %))}}]
+                        :or   {on-error   #(js/console.warn (.-error %))
+                               on-success #(js/console.log (.-data %))}}]
   (let [db (instantdb/init #js{:appId app-id})]
-    {:db db
+    {:db          db
      :unsubscribe (.subscribeQuery db #js{:todos #js{}}
                                    (fn [r]
                                      (if (.-error r)
@@ -37,32 +37,33 @@
                                  (let [{:keys [unsubscribe]} @!state]
                                    (when unsubscribe
                                      (unsubscribe))))
-       :reagent-render (fn []
-                         (let [{:keys [todos instantdb-app-id]} @!r-state
-                               {:keys [unsubscribe db]} @!state]
-                           [:div "DB Test"
-                            [:div {:style {:display        :flex}}
-                             "InstantDB app-id:"
-                             [ui/secret-edit-field {:on-save (fn [s]
-                                                               (when unsubscribe
-                                                                 (unsubscribe))
-                                                               (swap! !r-state assoc
-                                                                      :instantdb-app-id s
-                                                                      :todos nil)
-                                                               (js/localStorage.setItem "instantdb-app-id" s)
-                                                               (when (seq s)
-                                                                 (reset! !state
-                                                                         (init-instant-db
-                                                                           {:app-id     s
-                                                                            :on-success (make-instantdb-success-handler !r-state)}))))
+       :reagent-render         (fn []
+                                 (let [{:keys [todos instantdb-app-id]} @!r-state
+                                       {:keys [unsubscribe db]} @!state]
+                                   [:div
+                                    [:h3 "DB Test"]
+                                    [:div {:style {:display :flex}}
+                                     "InstantDB app-id:"
+                                     [ui/secret-edit-field {:on-save (fn [s]
+                                                                       (when unsubscribe
+                                                                         (unsubscribe))
+                                                                       (swap! !r-state assoc
+                                                                              :instantdb-app-id s
+                                                                              :todos nil)
+                                                                       (js/localStorage.setItem "instantdb-app-id" s)
+                                                                       (when (seq s)
+                                                                         (reset! !state
+                                                                                 (init-instant-db
+                                                                                   {:app-id     s
+                                                                                    :on-success (make-instantdb-success-handler !r-state)}))))
 
-                                                    :value   instantdb-app-id}]]
-                            [:ul
-                             (for [{:keys [id text]} todos]
-                               ^{:key id}
-                               [:li text])]
-                            [:button {:on-click #(.transact db
-                                                            (let [t (aget (.-todos ^js/Object (.-tx db)) (instantdb/id))]
-                                                              (.update t #js{:text (rand-nth ["foo" "bar" "hello" "ding dong" ":-D"])})))}
+                                                            :value   instantdb-app-id}]]
+                                    [:ul
+                                     (for [{:keys [id text]} todos]
+                                       ^{:key id}
+                                       [:li text])]
+                                    [:button {:on-click #(.transact db
+                                                                    (let [t (aget (.-todos ^js/Object (.-tx db)) (instantdb/id))]
+                                                                      (.update t #js{:text (rand-nth ["foo" "bar" "hello" "ding dong" ":-D"])})))}
 
-                             "Add item"]]))})))
+                                     "Add item"]]))})))
