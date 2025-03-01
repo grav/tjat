86e053e2cb115c261d8daade6ae393a1f97d3bb9
Mikkel Gravgaard <mikkel@klokke.dk>
2025-03-01 20:46:47 +0100

    WIP

--- app/src/tjat/db.cljs
+++ app/src/tjat/db.cljs
@@ -6,6 +6,7 @@
             [tjat.ui :as ui]))
 
 ;; https://www.instantdb.com/docs/start-vanilla
+;; we don't need no hooks!
 
 (defn init-instant-db [{:keys [app-id on-error on-success]
                         :or {on-error #(js/console.warn (.-error %))
@@ -18,40 +19,44 @@
                                        (on-error r)
                                        (on-success r))))}))
 
-
+(defn make-instantdb-success-handler [!r-state]
+  (fn [r]
+    (js/console.log (.-data r))
+    (swap! !r-state assoc :todos (js->clj
+                                   (aget (.-data r) "todos")
+                                   :keywordize-keys true))))
 
 (defn app []
-  (let [!r-state (r/atom nil)
-        !state (atom nil)]
+  (let [instantdb-app-id-persisted (js/localStorage.getItem "instantdb-app-id")
+        !r-state (r/atom {:instantdb-app-id instantdb-app-id-persisted})
+        !state (atom (when (seq instantdb-app-id-persisted)
+                       (init-instant-db {:app-id     instantdb-app-id-persisted
+                                         :on-success (make-instantdb-success-handler !r-state)})))]
     (r/create-class
       {:component-will-unmount (fn []
                                  (let [{:keys [unsubscribe]} @!state]
                                    (when unsubscribe
                                      (unsubscribe))))
        :reagent-render (fn []
-                         (let [{:keys [todos instant-db-app-id]} @!r-state
+                         (let [{:keys [todos instantdb-app-id]} @!r-state
                                {:keys [unsubscribe db]} @!state]
                            [:div "DB Test"
                             [:div {:style {:display        :flex}}
                              "InstantDB app-id:"
                              [ui/secret-edit-field {:on-save (fn [s]
-
                                                                (when unsubscribe
                                                                  (unsubscribe))
                                                                (swap! !r-state assoc
-                                                                      :instant-db-app-id s
+                                                                      :instantdb-app-id s
                                                                       :todos nil)
+                                                               (js/localStorage.setItem "instantdb-app-id" s)
                                                                (when (seq s)
                                                                  (reset! !state
                                                                          (init-instant-db
                                                                            {:app-id     s
-                                                                            :on-success (fn [r]
-                                                                                          (js/console.log (.-data r))
-                                                                                          (swap! !r-state assoc :todos (js->clj
-                                                                                                                         (aget (.-data r) "todos")
-                                                                                                                         :keywordize-keys true)))}))))
+                                                                            :on-success (make-instantdb-success-handler !r-state)}))))
 
-                                                    :value   instant-db-app-id}]]
+                                                    :value   instantdb-app-id}]]
                             [:ul
                              (for [{:keys [id text]} todos]
                                ^{:key id}
@@ -60,4 +65,4 @@
                                                             (let [t (aget (.-todos ^js/Object (.-tx db)) (instantdb/id))]
                                                               (.update t #js{:text (rand-nth ["foo" "bar" "hello" "ding dong" ":-D"])})))}
 
-                             "Add todo"]]))})))
+                             "Add item"]]))})))
