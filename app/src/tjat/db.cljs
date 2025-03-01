2c185a8ac75b5cc241df56fc2dee2f681cac7e9c
Mikkel Gravgaard <mikkel@klokke.dk>
2025-03-01 20:07:07 +0100

    WIP

--- app/src/tjat/db.cljs
+++ app/src/tjat/db.cljs
@@ -9,21 +9,25 @@
 (defn app []
   (let [db (instantdb/init #js{:appId "bb63ab72-4a57-4235-b8dc-93c07b60d2b3"})
         !state (r/atom nil)
-        ;; TODO - this runs on every mount, need unsubscribe!
-        _ (.subscribeQuery db #js{:todos #js{}}
-                           (fn [r]
-                             (if (.-error r)
-                               (js/console.warn (.-error r))
-                               (swap! !state assoc :todos (js->clj (aget (.-data r) "todos") :keywordize-keys true)))))]
-    (fn []
-      (let [{:keys [todos]} @!state]
-        [:div "DB Test"
-         [:ul
-          (for [{:keys [id text]} todos]
-            ^{:key id}
-            [:li text])]
-         [:button {:on-click #(.transact db
-                                         (let [t (aget (.-todos (.-tx db)) (instantdb/id))]
-                                           (.update t #js{:text (rand-nth ["foo" "bar" "hello" "ding dong" ":-D"])})))}
+        unsubscribe (.subscribeQuery db #js{:todos #js{}}
+                                     (fn [r]
+                                       (if (.-error r)
+                                         (js/console.warn (.-error r))
+                                         (do
+                                           (js/console.log (.-data r))
+                                           (swap! !state assoc :todos (js->clj (aget (.-data r) "todos") :keywordize-keys true))))))]
+    (r/create-class
+      {:component-will-unmount (fn []
+                                 (unsubscribe))
+       :reagent-render (fn []
+                         (let [{:keys [todos]} @!state]
+                           [:div "DB Test"
+                            [:ul
+                             (for [{:keys [id text]} todos]
+                               ^{:key id}
+                               [:li text])]
+                            [:button {:on-click #(.transact db
+                                                            (let [t (aget (.-todos (.-tx db)) (instantdb/id))]
+                                                              (.update t #js{:text (rand-nth ["foo" "bar" "hello" "ding dong" ":-D"])})))}
 
-          "Add todo"]]))))
+                             "Add todo"]]))})))
