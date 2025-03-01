185d4a3a7fff729121dda6407e60569966b02ec8
Mikkel Gravgaard <mikkel@klokke.dk>
2025-03-01 19:47:57 +0100

    WIP

--- app/src/tjat/db.cljs
+++ app/src/tjat/db.cljs
@@ -1,14 +1,29 @@
 (ns tjat.db
-  (:require ["@instantdb/react"
+  (:require ["@instantdb/core"
              :as instantdb
-             :refer [id i init InstaQLEntity]]))
+             :refer [id i init InstaQLEntity]]
+            [reagent.core :as r]))
+
+;; https://www.instantdb.com/docs/start-vanilla
 
 (defn app []
-  (let [db (instantdb/init #js{:app-id "bb63ab72-4a57-4235-b8dc-93c07b60d2b3"})
-        _ (.subscribeQuery #js{:todos {}}
+  (let [db (instantdb/init #js{:appId "bb63ab72-4a57-4235-b8dc-93c07b60d2b3"})
+        !state (r/atom nil)
+        ;; TODO - this runs on every mount, need unsubscribe!
+        _ (.subscribeQuery db #js{:todos #js{}}
                            (fn [r]
                              (if (.-error r)
                                (js/console.warn (.-error r))
-                               (js/console.log (.-data r)))))]
+                               (swap! !state assoc :todos (js->clj (aget (.-data r) "todos") :keywordize-keys true)))))]
     (fn []
-      [:div "DB Test"])))
+      (let [{:keys [todos]} @!state]
+        [:div "DB Test"
+         [:ul
+          (for [{:keys [id text]} todos]
+            ^{:key id}
+            [:li text])]
+         [:button {:on-click #(.transact db
+                                         (let [t (aget (.-todos (.-tx db)) (instantdb/id))]
+                                           (.update t #js{:text (rand-nth ["foo" "bar" "hello" "ding dong" ":-D"])})))}
+
+          "Add todo"]]))))
