f7a730dfc85f1c49e4708fca2629f6cb558b8a44
Mikkel Gravgaard <mikkel@klokke.dk>
2025-03-14 11:43:11 +0100

    WIP

--- app/src/tjat/algolia.cljs
+++ app/src/tjat/algolia.cljs
@@ -12,11 +12,25 @@
                      (clj->js {:indexName index-name
                                :body      {:text      "what size is the sun?"
                                            :id        4242
+                                           :objectID (str 4242) ;; important!
                                            :responses [{:model "model1"
                                                         :text  "quite big"
-                                                        :id    4243}]}}))
-        (.then #(.search client
-                         (clj->js {:requests [{:indexName index-name
-                                               :query "sun"}]})))
+                                                        :id    4243}
+                                                       #_{:model "model2"
+                                                          :text  "small compared to other stars"
+                                                          :id    4244}]}}))
+
         (.then js/console.log))))
+
+(comment
+  (let [app-id "4ORO0SDUY5"
+        api-key (js/localStorage.getItem "algolia-api-key")
+        index-name "test-index"
+        client (when (seq api-key)
+                 (algolia/algoliasearch app-id api-key))]
+    (-> (.search client
+                 (clj->js {:requests [{:indexName index-name
+                                       :query     "sun"}]}))
+        (.then js/console.log))))
+
 
