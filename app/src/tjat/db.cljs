b1d6da898582ac9f1bd573d01f2f81fdfdea7e03
Mikkel Gravgaard <mikkel@klokke.dk>
2025-08-26 21:51:11 +0200

    Instant DB support (#1)

---
+++ app/src/tjat/db.cljs
@@ -0,0 +1,31 @@
+(ns tjat.db
+  (:require ["@instantdb/core"
+             :as instantdb]
+            [allem.util :as util]))
+
+;; https://www.instantdb.com/docs/start-vanilla
+;; we don't need no hooks!
+
+(defn init-instant-db [{:keys [app-id subscriptions on-success on-error !state]
+                        :or   {on-error   #(js/console.warn (.-error %))}}]
+  (let [on-success (or on-success
+                       (and !state
+                            (fn [r]
+                              (swap! !state merge (js->clj (.-data r)
+                                                           :keywordize-keys true))))
+                       (throw (ex-info "Neither on-success nor !state was given to init-instant-db!"
+                                       {})))
+
+        db (instantdb/init #js{:appId app-id})
+        unsubscribe-fn (.subscribeQuery db (clj->js subscriptions)
+                                        (fn [r]
+                                          (if (.-error r)
+                                            (on-error r)
+                                            (on-success r))))]
+    {:db          db
+     :unsubscribe (fn []
+                    (when !state
+                      (swap! !state #(apply dissoc % (keys subscriptions))))
+                    (unsubscribe-fn))}))
+
+
