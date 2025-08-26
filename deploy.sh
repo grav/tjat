cd0e9a67be845af1a7af4fda9a4c9b9e508464ed
Mikkel Gravgaard <mikkel@klokke.dk>
2025-08-26 21:51:12 +0200

    R2 + S3 api compatibility

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,8 @@ aws s3api put-object \
   --endpoint-url "$S3_ENDPOINT_URL" \
   --bucket "$S3_BUCKET" \
   --key "${dir}/js/main.js" --body public-release/js/main.js  \
-  --acl public-read
+  --acl public-read \
+  --checksum-algorithm CRC32
 
 index_file="$(mktemp).html"
 
@@ -26,6 +27,7 @@ aws s3api put-object \
   --endpoint-url "$S3_ENDPOINT_URL" \
   --bucket "$S3_BUCKET" \
   --key "${dir}/index.html" --body "$index_file" \
-  --acl public-read
+  --acl public-read \
+  --checksum-algorithm CRC32
 
 popd
