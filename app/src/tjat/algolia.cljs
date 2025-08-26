0abfdb5f4a4a6285a2842d3dfa560ee88722ee1b
Mikkel Gravgaard <mikkel@klokke.dk>
2025-08-26 21:51:12 +0200

    Search: Algolia (#2)

---
+++ app/src/tjat/algolia.cljs
@@ -0,0 +1,40 @@
+(ns tjat.algolia
+  (:require ["algoliasearch" :as algolia]))
+
+(def index-name-chats "tjat-chats")
+
+(def index-name-responses "tjat-responses")
+
+;; https://www.algolia.com/doc/libraries/javascript/v5/
+(comment
+  (let [app-id "4ORO0SDUY5"
+        api-key (js/localStorage.getItem "algolia-api-key")
+        index-name "test-index"
+        client (when (seq api-key)
+                 (algolia/algoliasearch app-id api-key))]
+    (-> (.saveObject client
+                     (clj->js {:indexName index-name
+                               :body      {:text      "what size is the sun?"
+                                           :id        4242
+                                           :objectID (str 4242) ;; important!
+                                           :responses [{:model "model1"
+                                                        :text  "quite big"
+                                                        :id    4243}
+                                                       #_{:model "model2"
+                                                          :text  "small compared to other stars"
+                                                          :id    4244}]}}))
+
+        (.then js/console.log))))
+
+(comment
+  (let [app-id "4ORO0SDUY5"
+        api-key (js/localStorage.getItem "algolia-api-key")
+        index-name "test-index"
+        client (when (seq api-key)
+                 (algolia/algoliasearch app-id api-key))]
+    (-> (.search client
+                 (clj->js {:requests [{:indexName index-name
+                                       :query     "sun"}]}))
+        (.then js/console.log))))
+
+
