ad69dafa5209c1b0cf8ec5f9453185f549cbf14e
Mikkel Gravgaard <mikkel@klokke.dk>
2025-08-26 21:51:11 +0200

    Chat history

--- app/src/tjat/core.cljs
+++ app/src/tjat/core.cljs
@@ -6,7 +6,6 @@
             [allem.core]
             [httpurr.client :as http]
             [httpurr.client.xhr-alt :refer [client]]
-            [tjat.markdown :as markdown]
             ["showdown" :as showdown]))
 
 (defonce root (createRoot (gdom/getElement "app")))
@@ -72,7 +71,51 @@
                                  (swap! !state assoc :is-editing? false :text nil))}
             "Save"])]))))
 
+(defn response-view [{:keys [time id response]}]
+  (when id
+    [:div
+     [:div [:i (str time)]]
+     [:div [:p
+            {:dangerouslySetInnerHTML
+             {:__html (.makeHtml
+                        ;; https://github.com/showdownjs/showdown?tab=readme-ov-file#valid-options
+                        (doto (showdown/Converter.)
+                          (.setFlavor "github")) response)}}]]]))
 
+(defn response-tabs [{:keys [selected-chat selections chats]}
+                     {:keys [on-response-select]}]
+  (let [responses (get chats selected-chat)
+        {selected-response-id selected-chat} selections]
+    [:div {:style {:display :flex}}
+     (for [[i {:keys [model id] :as v}] (map vector (range) (sort-by :time responses))]
+       ^{:key i} [:div [:div
+                        {:style {:padding 10}
+                         :on-click #(on-response-select [selected-chat id])}
+                        [:div {:style {:background-color (when (= selected-response-id id) :lightgray)}}
+                         (name model)]]])]))
+
+
+(defn chat-menu [{:keys [chats selected-chat selections]
+                  :as state}
+                 {:keys [on-chat-select]
+                  :as handlers}
+                 !state]
+  (let [{selected-response-id selected-chat} selections]
+    [:div {:style {:display :flex
+                   :padding 10}}
+     [:div (for [[k _] chats]
+             ^{:key k} [:div {:on-click #(on-chat-select k)}
+                        [:div {:style {:font-weight      900
+                                       :background-color (when (= selected-chat k) :lightgray)
+                                       :padding          10
+                                       :width        100}} k]
+                        (when (= selected-chat k))])]
+     [:div {:style {:padding 10}}
+      [response-tabs state handlers]
+      [:hr]
+      [response-view (->> (get chats selected-chat)
+                          (filter (comp #{selected-response-id} :id))
+                          util/single)]]]))
 
 (defn app []
   (let [api-keys-persisted (some-> (js/localStorage.getItem "tjat-api-keys")
@@ -127,8 +170,17 @@
                                                     :model    model
                                                     :api-keys api-keys})
                                       (.then (fn [v]
-                                               (swap! !state assoc :answer v
-                                                      :loading false)))))}
+                                               (let [id (random-uuid)]
+                                                 ;; TODO: potentially do this in one 'swap'
+                                                 (swap! !state update-in [:chats text] conj {:id id
+                                                                                             :model model
+                                                                                             :time (js/Date.)
+                                                                                             :response v})
+                                                 (swap! !state assoc-in [:selections text] id)
+                                                 (swap! !state assoc
+                                                        :loading false
+                                                        :selected-chat text))))))}
+
             "submit"]
            (when loading
              [:svg.spinner
@@ -142,12 +194,14 @@
                 :fill         "none"
                 :stroke       "#007bff"
                 :stroke-width "4"}]])]
-          [:p
-           {:dangerouslySetInnerHTML {:__html (.makeHtml
-                                                (doto (showdown/Converter.
-                                                        ;; https://github.com/showdownjs/showdown?tab=readme-ov-file#valid-options
-                                                        #_(clj->js {:tables true}))
-                                                  (.setFlavor "github")) answer)}}]]]))))
+          [chat-menu @!state
+           {:on-chat-select (fn [selected-chat]
+                              (swap! !state assoc
+                                     :selected-chat selected-chat
+                                     ;; TODO - this might be a bit aggressive ...
+                                     :text selected-chat))
+            :on-response-select (fn [[selected-chat id]]
+                                  (swap! !state assoc-in [:selections selected-chat] id))}]]]))))
 
 #_(defn testit []
     [:div
