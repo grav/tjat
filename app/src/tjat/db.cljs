cf9b2da108adf6f36520dc2486dd0f25c0ff5d62
Mikkel Gravgaard <mikkel@klokke.dk>
2025-10-10 11:38:17 +0200

    Better InstantDB error handling

--- app/src/tjat/db.cljs
+++ app/src/tjat/db.cljs
@@ -7,7 +7,7 @@
 ;; we don't need no hooks!
 
 (defn init-instant-db [{:keys [app-id subscriptions on-success on-error !state]
-                        :or   {on-error   #(js/console.warn (.-error %))}}]
+                        :or   {on-error #(js/console.log (.-error %))}}]
   (let [on-success (or on-success
                        (and !state
                             (fn [r]
@@ -16,12 +16,15 @@
                        (throw (ex-info "Neither on-success nor !state was given to init-instant-db!"
                                        {})))
 
-        db (instantdb/init #js{:appId app-id})
-        unsubscribe-fn (.subscribeQuery db (clj->js subscriptions)
-                                        (fn [r]
-                                          (if (.-error r)
-                                            (on-error r)
-                                            (on-success r))))]
+        db (try
+             (instantdb/init #js{:appId app-id})
+             (catch js/Error e
+               (js/alert e)))
+        unsubscribe-fn ^js/Function (.subscribeQuery db (clj->js subscriptions)
+                                                     (fn [r]
+                                                       (if (.-error r)
+                                                         (on-error r)
+                                                         (on-success r))))]
     {:db          db
      :unsubscribe (fn []
                     (when !state
